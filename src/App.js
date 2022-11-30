be131e07aacc73f18201b9fd8c6791a1c3638070
Klippie <info@johanvanderklift.nl>
2022-11-30 16:24:13 +0100

    Edhub 6.4

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import SearchBar from './components/searchBar/SearchBar';
 import TabBarMenu from './components/tabBarMenu/TabBarMenu';
 import MetricSlider from './components/metricSlider/MetricSlider';
+import axios from "axios";
+
+const apiKey = '73c57ea6d7d190039dd9820a17d06ee1';
 
 function App() {
+  const [weatherData, setWeatherData] = useState({});
+
+  async function fetchData() {
+    try {
+      const result = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=utrecht,nl&appid=${apiKey}&lang=nl`);
+      console.log(result.data);
+      setWeatherData(result.data);
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
   return (
     <>
       <div className="weather-container">
@@ -14,11 +29,17 @@ function App() {
           <SearchBar/>
 
           <span className="location-details">
-            <h2>Bewolkt</h2>
-            <h3> </h3>
-            <h1>14 &deg;</h1>
-
-            <button type="button">
+            {Object.keys(weatherData).length > 0 &&
+            <>
+              <h2>{weatherData.weather[0].description}</h2>
+              <h3>{weatherData.name}</h3>
+              <h1>{weatherData.main.temp}</h1>
+            </>
+            }
+            <button
+              type="button"
+              onClick={fetchData}
+            >
               Haal data op!
             </button>
           </span>
